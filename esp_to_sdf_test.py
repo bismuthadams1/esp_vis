656c45914181b6fd9df584c46ab57172d52281f2
bismuthadams1 <bismuthadams@gmail.com>
2025-02-18 09:19:20 +0000

    modified env file

--- esp_to_sdf_test.py
+++
@@ -1,9 +0,0 @@
-from esp_to_sdf import ESPtoSDF   
-
-
-def main():
-    esp_from_sdf = ESPtoSDF()
-    esp_from_sdf.make_charge_sdf(sdf_file="/Users/localadmin/Documents/Documents - CMDADMIN’s MacBook Air/visualize_esp_public/Theophylline.sdf")
-
-if __name__ == "__main__":
-    main()
