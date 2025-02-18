b8726795f9b39e23443186eac359648e24496137
Charlie Adams <64149298+bismuthadams1@users.noreply.github.com>
2025-02-18 09:43:46 +0000

    Update esp_to_sdf_test.py

--- esp_to_sdf_test.py
+++ esp_to_sdf_test.py
@@ -3,7 +3,7 @@ from esp_to_sdf import ESPtoSDF
 
 def main():
     esp_from_sdf = ESPtoSDF()
-    esp_from_sdf.make_charge_sdf(sdf_file="/Users/localadmin/Documents/Documents - CMDADMIN’s MacBook Air/visualize_esp_public/Theophylline.sdf")
+    esp_from_sdf.make_charge_sdf(sdf_file="./Theophylline.sdf")
 
 if __name__ == "__main__":
     main()
